f36a3f7b2f0243f06fdb28933bd2bf3ebb0f2a62
Prateek Bansal <55333063+ItsPrateekBansal@users.noreply.github.com>
2020-03-29 05:47:45 +0530

    Create LongestCommonSubsequence.cpp

---
+++ LongestCommonSubsequence.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int lcs_dp(string s,string t)
+{
+    int m = s.size();
+    int n = t.size();
+    int **output = new int*[m+1];
+    for(int i = 0;i<m+1;i++)
+    {
+        output[i] = new int[n+1];
+    }
+    output[0][0] = 0;
+    for(int i=0;i<=m;i++)
+    {
+        output[i][0] = 0;
+    }
+    for(int i=0;i<=n;i++)
+    {
+        output[0][i] = 0;
+    }
+    for(int i = 1; i <= m; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+             if(s[m-i] == t[n-j])
+             {
+                 output[i][j] = 1 + output[i-1][j-1];
+             }
+             else
+             {
+                 int a = output[i-1][j];
+                 int b = output[i-1][j-1];
+                 int c = output[i][j-1];
+                 output[i][j] = max(a,max(b,c));
+             }
+             
+        }
+    }
+    return output[m][n];
+}
+
+
+
+int lcs(string s, string t) {
+		// Base case
+	if(s.size() == 0 || t.size() == 0) {
+		return 0;
+	}
+
+		// Recursive calls
+	if(s[0] == t[0]) {
+		return 1 + lcs(s.substr(1), t.substr(1));
+	}
+	else {
+
+		int a = lcs(s.substr(1), t);
+		int b = lcs(s, t.substr(1));
+		int c = lcs(s.substr(1), t.substr(1));
+		return max(a, max(b, c));
+	}
+}
+
+int main() {
+	string s, t;
+	cin >> s >> t;
+
+	//cout << lcs(s, t) << endl;
+    cout << lcs_dp(s, t) << endl;
+}
